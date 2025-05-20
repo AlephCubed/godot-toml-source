c32367c1ab34d50109b929e452ea54f1f5a21ff1
AlephCubed <jothebro500@gmail.com>
2025-05-19 20:51:18 -0700

    Update to GDExtension and start on `stringify` method.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,22 @@
-#[macro_use]
-extern crate gdnative;
-
 mod godot_toml;
 
-fn init(handle: gdnative::init::InitHandle) {
-	handle.add_class::<godot_toml::GodotToml>();
-}
+use crate::godot_toml::GodotToml;
+use godot::classes::Engine;
+use godot::prelude::*;
 
-godot_gdnative_init!();
-godot_nativescript_init!(init);
-godot_gdnative_terminate!();
+struct TomlExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for TomlExtension {
+    fn on_level_init(level: InitLevel) {
+        if level == InitLevel::Scene {
+            Engine::singleton().register_singleton("TOML", &GodotToml::new_alloc());
+        }
+    }
+
+    fn on_level_deinit(level: InitLevel) {
+        if level == InitLevel::Scene {
+            Engine::singleton().unregister_singleton("TOML");
+        }
+    }
+}
